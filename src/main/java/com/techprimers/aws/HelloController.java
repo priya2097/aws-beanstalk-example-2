7c53b9e52995212ce4fec903483781ce60fa59cc
priya2097 <56018590+priya2097@users.noreply.github.com>
2019-10-24 12:05:17 +0530

    Update HelloController.java

--- src/main/java/com/techprimers/aws/HelloController.java
+++ src/main/java/com/techprimers/aws/HelloController.java
@@ -4,12 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/hello")
+@RequestMapping("/prodevans")
 @RestController
 public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "Hello TechPrimers";
+        return "Welcome to prodevans";
     }
 }
